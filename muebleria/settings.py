dc4e917e0710896949137da337d8d05fcc2838c4
gabrielabarrionuevo <gabrielabe97@gmail.com>
2024-07-09 20:36:09 -0300

    Preparacion para despliegue

--- muebleria/settings.py
+++ muebleria/settings.py
@@ -9,12 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&=@mml*l$#_l51f(p1*ap@4y=k&6dyrw%5rgk9px&ahhg9dg+#'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-&=@mml*l$#_l51f(p1*ap@4y=k&6dyrw%5rgk9px&ahhg9dg+#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -47,6 +47,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
