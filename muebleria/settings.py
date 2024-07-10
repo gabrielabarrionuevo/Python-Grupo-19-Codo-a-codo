2e68c0c571410c23431d17442b26f48442924cfb
gabrielabarrionuevo <gabrielabe97@gmail.com>
2024-07-10 01:28:44 -0300

    corrijo error static dir

--- muebleria/settings.py
+++ muebleria/settings.py
@@ -49,8 +49,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'muebleriaApp', 'static'),
 ]
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
